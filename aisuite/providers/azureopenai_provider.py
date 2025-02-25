7bac681af010a8f8843625ee8ed20933e4ef8ff4
Ming Gong <ming@neuron.sg>
2025-02-25 12:21:02 +0800

    MG: Allow max_retries for AzureOpenAI provider.

--- aisuite/providers/azureopenai_provider.py
+++ aisuite/providers/azureopenai_provider.py
@@ -1,6 +1,6 @@
 import os
 
-from openai import AzureOpenAI, AsyncAzureOpenAI
+from openai import AzureOpenAI, AsyncAzureOpenAI, DEFAULT_MAX_RETRIES
 
 from aisuite.provider import Provider
 
@@ -11,11 +11,16 @@ class AzureopenaiProvider(Provider):
         self.api_version = config.get("api_version") or os.getenv("AZURE_OPENAI_API_VERSION")
         self.api_key = config.get("api_key") or os.getenv("AZURE_OPENAI_API_KEY")
 
+        self.max_retries = config.get("max_retries") or os.getenv("AZURE_OPENAI_MAX_RETRIES")
+        if not self.max_retries:
+            self.max_retries = DEFAULT_MAX_RETRIES
+
         if not self.api_key or not self.base_url or not self.api_version:
             raise ValueError("For Azure OpenAI, api_key, azure_endpoint, api_version are required.")
 
-        self.client = AzureOpenAI(api_key=self.api_key, api_version=self.api_version, azure_endpoint=self.base_url)
-        self.async_client = AsyncAzureOpenAI(api_key=self.api_key, api_version=self.api_version, azure_endpoint=self.base_url)
+
+        self.client = AzureOpenAI(api_key=self.api_key, api_version=self.api_version, azure_endpoint=self.base_url, max_retries= self.max_retries)
+        self.async_client = AsyncAzureOpenAI(api_key=self.api_key, api_version=self.api_version, azure_endpoint=self.base_url, max_retries= self.max_retries)
 
 
     async def async_chat_completions_create(self, deployment_name, messages, **kwargs):
